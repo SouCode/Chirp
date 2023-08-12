a2191416150f190edc9d1fe45a5016499b8e54fb
Sou <samelsonjohn@protonmail.com>
2023-08-12 11:10:14 -0400

    adjusted Text Area Responsiveness

--- linpass/src/components/NewTweetForm.tsx
+++ linpass/src/components/NewTweetForm.tsx
@@ -1,24 +1,43 @@
+import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
 
-export function NewTweetForm() {
-    const session = useSession()
-    const [inputValue, setInputValue] = useState("")
+function updateTextAreaSize(textArea: HTMLTextAreaElement) {
+    textArea.style.height = "0";
+    textArea.style.height = `${textArea.scrollHeight}px`;
+}
 
-    if (session.status !== "authenticated") return
+export function NewTweetForm() {
+    const session = useSession();
+    const [inputValue, setInputValue] = useState("");
+    const textAreaRef = useRef<HTMLTextAreaElement | null>(null); // Add type union with null
+
+    const inputRef = useCallback((textArea: HTMLTextAreaElement | null) => {
+        if (textArea) {
+            updateTextAreaSize(textArea);
+            textAreaRef.current = textArea;
+        }
+    }, []);
+
+    useLayoutEffect(() => {
+        if (textAreaRef.current) {
+            updateTextAreaSize(textAreaRef.current);
+        }
+    }, [inputValue]);
+
+    if (session.status !== "authenticated") return null;
 
     return (
-        <form className="flex flex-col gap-2 border-b px4 py-2">
+        <form className="flex flex-col gap-2 border-b px-4 py-2">
             <div className="flex gap-4">
                 <ProfileImage src={session.data.user.image} /> 
                 <textarea 
-                style={{ height: 0}}
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" 
-                placeholder="Whats Happening?"
+                    ref={inputRef}
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" 
+                    placeholder="What's Happening?"
                 />
             </div>                
             <Button className="self-end">Tweet</Button>  
