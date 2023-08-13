74cad116df0b1d739394abcd41aa4e07866e54bf
Sou <samelsonjohn@protonmail.com>
2023-08-13 02:07:29 -0400

    Red hover and like count added

--- linpass/src/components/infiniteTweetList.tsx
+++ linpass/src/components/infiniteTweetList.tsx
@@ -100,11 +100,25 @@ function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
     }
 
     return (
-        <button className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
-            <HeartIcon />
+        <button
+            className={`mb-1 mt-1 flex items-center gap-3 self-start transition-colors
+                duration-200 ${
+                    likedByMe
+                        ? "text-red-500"
+                        : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+                }`}
+        >
+            <HeartIcon
+                className={`transition-colors duration-200 ${
+                    likedByMe
+                        ? "fill-red-500"
+                        : "group-hover:fill-red-500 group-focus-visible:fill-red-500"
+                }`}
+            />
             <span>{likeCount}</span>
         </button>
     );
+    
 }
 
 export default InfiniteTweetList;
