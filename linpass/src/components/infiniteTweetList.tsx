e55f6bb7b1ef9fcc831d1a4412164eccd6c55572
Sou <samelsonjohn@protonmail.com>
2023-08-12 23:39:57 -0400

    RecentTweet User and DateTime style added

--- linpass/src/components/infiniteTweetList.tsx
+++ linpass/src/components/infiniteTweetList.tsx
@@ -1,4 +1,6 @@
 import InfiniteScroll from "react-infinite-scroll-component";
+import { ProfileImage } from "./ProfileImage";
+import Link from "next/link";
 
 type Tweet = {
     id: string;
@@ -48,8 +50,33 @@ export function InfiniteTweetList({
         </InfiniteScroll>
     </ul>
     );
+}
+
+    const dateTimeFormatter = new Intl.DateTimeFormat(undefined, { dateStyle:
+        "short" })
+
 
     function TweetCard({ id, user, content, createdAt, likeCount, likedByMe}: Tweet) {
-        return <li className="flex gap-4 border-b px-4 py-4">{content}</li>
+        return (
+            <li className="flex gap-4 border-b px-4 py-4">
+                <Link href={`/profiles/${user.id}`}>
+                    <ProfileImage src={user.image} />
+                </Link>
+                <div className="flex flex-grow flex-col">
+                    <div className="flex gap-1">
+                        <Link
+                            href={`/profiles/${user.id}`}
+                            className="font-bold outline-none hover:underline
+                            focus-visible:underline"
+                        >
+                            {user.name}
+                        </Link>
+                        <span className="text-gray-500">-</span>
+                        <span className="text-gray-500">{dateTimeFormatter.format(createdAt)}</span>
+
+                    </div>
+
+                </div>
+            </li>
+        );
     }
- }
